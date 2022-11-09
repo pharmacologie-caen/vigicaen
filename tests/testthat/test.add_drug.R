9d33387a4145351a78aae34360fd11ddaddb293a
cdolladille <cdolladille@hotmail.com>
2022-11-09 17:32:48 +0100

    commit details on test purpose

--- tests/testthat/test.add_drug.R
+++ tests/testthat/test.add_drug.R
@@ -104,8 +104,10 @@ test_that("selecting only s, c, i works and provide less cases than sci altogeth
 
   n_drug <- length(d_names)
 
+  bas <- c("s", "c", "i")
+
   res_each <-
-    purrr::map(c("s", "c", "i"), function(repbasis_)
+    purrr::map(bas, function(repbasis_)
       demo %>%
         add_drug(
           exposure_df = ex_$d_drecno,
@@ -136,6 +138,7 @@ test_that("selecting only s, c, i works and provide less cases than sci altogeth
   expect_equal(ncol(demo),
                ncol(demo_) + n_drug)
 
+  # found cases with s, c, i
   purrr::walk(
     res_each_nivo_sum,
     function(x){
@@ -144,8 +147,14 @@ test_that("selecting only s, c, i works and provide less cases than sci altogeth
     }
   )
 
+  # found equally or more cases with s, c, i alone and sci altogether (see below)
   expect_gte(sum(res_each_nivo_sum),
              res_all_nivo_sum)
+
+  # found different number of reports with s, c, i
+  expect_equal(res_each_nivo_sum[1] == res_each_nivo_sum[2], FALSE)
+  expect_equal(res_each_nivo_sum[2] == res_each_nivo_sum[3], FALSE)
+  expect_equal(res_each_nivo_sum[1] == res_each_nivo_sum[3], FALSE)
 
   # additional details : why is it ok to have MORE reports with sum(res_each_nivo_sum) ? Because some reports have the same drug reported with 2 or 3 repbasis (i.e. suspect and interacting, concomitant and interacting)
 
