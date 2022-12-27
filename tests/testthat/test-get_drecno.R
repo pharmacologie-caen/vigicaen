81fdfbe8ea4df814b91976bc461a89c45b5db355
cdolladille <117442633+cdolladille@users.noreply.github.com>
2022-12-27 12:35:04 +0100

    WIP creating get_drecno

---
+++ tests/testthat/test-get_drecno.R
@@ -0,0 +1,9 @@
+test_that("get drecno of a single drug, no combination allowed", {
+
+  d_sel_names <- list2(
+   nivolumab = "nivolumab",
+   ipilimumab = "ipilimumab",
+   nivo_ipi = c("nivolumab", "ipilimumab")
+   )
+
+})
