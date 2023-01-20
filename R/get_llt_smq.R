1dfde6d01fc2067150dab470daa8b28e8ba84f21
cdolladille <117442633+cdolladille@users.noreply.github.com>
2023-01-20 16:20:18 +0100

    add get_llt_smq

---
+++ R/get_llt_smq.R
@@ -0,0 +1,171 @@
+#' Extract llts from SMQs
+#'
+#' Extracts from a smq_list_content a list of llts given an SMQ.
+#'
+#' !! You must use this function only for NON-algorithmic SMQs (this status is given in the smq_list table). There are 2 non-blocking checkers in the function. Be sure to add ` (SMQ)` at the end of the SMQ name. The other checker is when nothing is found.
+#'
+#' @param smq A character vector, length 1.
+#' @param smq_scope A character vector. One of "narrow" or "broad".
+#' @param smq_list_content A data.table. A joint of smq_list and smq_content
+#' @keywords meddra smq llt
+#' @export
+#' @examples
+#' ## Finding llt codes for Embolism (SMQ)
+#' smq_sel <- rlang::list2(
+#'   embolism = "Embolic and thrombotic events, venous (SMQ)"
+#'  )
+#' get_llt_smq(smq_sel,
+#'                 smq_scope = "narrow",
+#'                 smq_list_content = ex_[["smq_list_content"]]
+#'                 )
+
+get_llt_smq <-
+  function(
+    smq,
+    smq_scope = c("narrow", "broad"),
+    smq_list_content
+  ){
+    # if(!grepl(" (SMQ)$", smq)) {
+    #   stop("smq name not ending by ` (SMQ)`")
+    # }
+    # smq <-
+    #   smq %>%
+    #   rlang::set_names(function(.x)
+    #     if_else(
+    #       stringr::str_detect(.x, "\\s(SMQ)$"),
+    #       .x,
+    #       paste0(.x, " (SMQ)")
+    #     ))
+
+    smq <-
+      purrr::map(smq, function(s_)
+      if_else(
+        stringr::str_detect(s_, "\\s\\(SMQ\\)$"),
+        s_,
+        paste0(s_, " (SMQ)")
+      )
+      )
+
+    smq_scope <- match.arg(smq_scope)
+
+    smq_scope_code <-
+      dplyr::case_when(
+        smq_scope == "narrow" ~ "2",
+        smq_scope == "broad" ~ "1",
+        TRUE ~ "this is an error"
+      )
+
+    get_one_smq_llt <- function(one_smq){
+      smq_list_content[smq_name == one_smq &
+                         term_scope == smq_scope_code &
+                         term_status == "A" &
+                         # A pour terme actif
+                         smq_algorithm == "N",
+                       # N : not algorithmic
+
+                       unique(term_code)
+                       ]
+    }
+
+    llt_list <- purrr::map(smq, get_one_smq_llt)
+
+    length_llt_list_element <-
+      purrr::map(llt_list, length)
+
+    # Handling 0 results issues ----
+
+    if(any(length_llt_list_element == 0)){
+
+      # debugging steps
+
+      zero_llt_element <-
+        length_llt_list_element %>%
+        purrr::keep(~ .x == 0)
+
+      zero_llt_element_names <-
+        names(zero_llt_element)
+
+      # step 1 : are there high level SMQs ?
+
+      high_level_smq <-
+        smq[zero_llt_element_names] %>%
+        purrr::map(function(zero_s){
+          # if you find zero_s in the list of smqs
+          zero_s %in% unique(smq_list_content$smq_name) &&
+            # and those smqs are not algorithmic
+            smq_list_content[smq_name == zero_s, all(smq_algorithm == "N")]
+        }) %>%
+        purrr::keep(isTRUE)
+
+       if(length(high_level_smq) > 0)
+        warning(paste0("In '",
+                       names(high_level_smq),
+                       "', at least one of '",
+                       paste0(smq[names(high_level_smq)], collapse = ", "),
+                       "' is/are high level smq(s), you need to query lower level smq(s)"))
+
+      # step 2 : are there algorithmic smq?
+
+      non_hls_zerollt_element <-
+        zero_llt_element_names[!zero_llt_element_names %in%
+                                 names(high_level_smq)]
+
+
+      smq_algorithm_not_n <-
+        smq[non_hls_zerollt_element] %>%
+        purrr::map(function(s_)
+          s_ %in% unique(smq_list_content$smq_name) &&
+          smq_list_content[smq_name == s_ &
+                             term_status == "A",
+                           all(!(smq_algorithm == "N"))
+          ]
+        ) %>%
+        purrr::keep(isTRUE)
+
+      if(length(smq_algorithm_not_n) > 0)
+        # this should be an error, as it is out of the function scope
+        stop(paste0("smq '",
+
+                    paste0(names(smq_algorithm_not_n), collapse = ", "),
+                    "' is/are algorithmic, they are not handled by get_llt_smq."))
+
+      # in all other cases, check for unmatched
+
+      # non_hls_non_algo_still_zero <-
+      #   non_hls_zerollt_element[!non_hls_zerollt_element %in%
+      #                             names(smq_algorithm_not_n)]
+      #
+      #
+      # if(length(non_hls_non_algo_still_zero) > 0)
+      #   warning(paste0("no llt code found for '",
+      #                  paste0(non_hls_non_algo_still_zero, collapse = ", "),
+      #                  "'. Check spelling?"
+      #                  ))
+    }
+
+    # check for unmatched terms (its smarter than the previous, since is does
+    # capture an unmatched term in the middle of good ones)
+
+    get_unmatched_terms <- function(one_smq){
+      unmatch_request <-
+        rlang::expr(one_smq[!(!!one_smq %in% smq_list_content[, unique(smq_name)]
+        )])
+
+      unmatch <- eval(unmatch_request)
+
+      unmatch
+    }
+
+    um_term <- purrr::map(smq, get_unmatched_terms) %>%
+      purrr::compact()
+
+    if(length(um_term) > 0)
+      warning(paste0("In '",
+                     paste0(names(um_term), collapse = ", "),
+                     "', the following elements were not found: ",
+                     paste0(unlist(um_term), collapse = ", "),
+                     ". Check spelling."))
+
+    llt_list
+
+  }
---
+++ tests/testthat/test-get_llt_smq.R
@@ -0,0 +1,92 @@
+test_that("there is no duplicate in extracted llts", {
+  # Intended for the rechallenge data management
+
+  llt_extraction_smq <-
+    get_llt_smq(
+      "Embolic and thrombotic events, venous (SMQ)",
+      smq_list_content = ex_$smq_list_content)
+
+  expect_equal(length(llt_extraction_smq),
+               length(unique(llt_extraction_smq)))
+})
+
+test_that("find the appropriate number of codes", {
+  smq_sel <- rlang::list2(
+      embolism = "Embolic and thrombotic events, venous (SMQ)"
+     )
+
+
+  smq_res_length <- rlang::list2(embolism = 324)
+
+  adr_llt <-
+    get_llt_smq(smq_sel,
+                smq_scope = "narrow",
+                smq_list_content = ex_[["smq_list_content"]])
+
+  purrr::iwalk(adr_llt,
+               function(p, p_n)
+                 expect_equal(length(p), smq_res_length[[p_n]]))
+})
+
+test_that("omitting ' (SMQ)' is corrected", {
+  smq_sel <- rlang::list2(
+    embolism = "Embolic and thrombotic events, venous (SMQ)",
+    embolism2 = "Embolic and thrombotic events, venous"
+  )
+
+  r1 <-
+    get_llt_smq(smq_sel,
+                smq_scope = "narrow",
+                smq_list_content = ex_[["smq_list_content"]])
+
+  expect_equal(r1[["embolism"]], r1[["embolism2"]])
+})
+
+test_that("errors and warnings pop as needed", {
+
+  smq_sel <- rlang::list2(
+    embolism = "Embolic and thrombotic events, venous"
+  )
+
+  s1 <- rlang::list2(
+    wrong2 = "Ischaemic heart disease (SMQ)"
+  )
+
+  s_algorithmic <- rlang::list2(
+    algo = "Neuroleptic malignant syndrome (SMQ)"
+  )
+
+  s2 <- rlang::list2(
+    wrong_alone = "ouuioui (SMQ)"
+  )
+
+  s3 <- rlang::list2(
+    wrong_inassociationwithagoodone = "ouuioui (SMQ)",
+    good = "Embolic and thrombotic events, venous (SMQ)"
+  )
+
+  expect_warning(
+    get_llt_smq(s1,
+                smq_list_content = ex_[["smq_list_content"]]),
+    "high level smq"
+  )
+
+  expect_error(
+    get_llt_smq(s_algorithmic,
+                smq_list_content = ex_[["smq_list_content"]]),
+    "algorithmic"
+  )
+
+  expect_warning(
+    get_llt_smq(s2,
+                smq_list_content = ex_[["smq_list_content"]]),
+    "the following elements were not found: ouuioui"
+  )
+
+  expect_warning(
+    get_llt_smq(s3,
+                smq_list_content = ex_[["smq_list_content"]]),
+    "the following elements were not found: ouuioui"
+  )
+
+})
