140327d8743a659c01d4e81c3211ac665ef82558
cdolladille <cdolladille@hotmail.com>
2023-11-20 00:52:18 +0100

    update test of rch_desc (luda_ was regenerated with a random component)

--- R/desc_rch.R
+++ R/desc_rch.R
@@ -3,26 +3,36 @@
 #' drug-adr pair description of rechallenge cases and time to onsets.
 #'
 #' Description span from rechallenge cases to __informative__ rechallenge cases (those cases where the outcome is known). Drug and Adr identifiers refer to DrecNo and MedDRA_Id, respectively.
-#' Terminology :
-#' "overall" as opposed to "rch" for rechallenged "rch" "no_rch" "overall".
-#' Among "rch", "inf" as opposed to "non_inf" for informatif ("non_inf" not used)
-#' Among "inf", rec" as opposed to "no_rec".
+#' Terminology
+#' \itemize{
+#'   \item `Overall` as opposed to `rch` for rechallenged (`rch` + `no_rch` = `overall`).
+#'   \item Among `rch`, `inf` (informative) as opposed to `non_inf` (`inf` + `non_inf` = `rch`)
+#'   \item Among `inf`, `rec` (recurring) as opposed to `non_rec` (`rec` + `non_rec` = `inf`)
+#' }
+#'
+#'
+#'
 #' While it might look surprising to have 2 different purposes in the same function (e.g. describe rechallenge and time to onset), it happens data management steps are almost identical. In a bright future, the function might be splitted in 2.
 #'
-#' @param luda_data # A data.table. luda stands for Link with UmcreportId, Drug and Adr identifiers (see details).
-#' @param demo_data # A data.table. demo should have a serious and death columns.
-#' @param adr_s # A character string. The name of the adr column. Adr columns can be created with \code{\link{add_adr}} in a luda table.
-#' @param drug_s # A character string. The name of the drug column. Drug columns can be created with \code{\link{add_drug}} in a luda table.
-#' @param tto_time_range # Incertitude range of Time to onset, in days. Defaults to 1 as recommended by umc
+#' @param luda_data A data.table. luda stands for Link with UmcreportId, Drug and Adr identifiers (see details).
+#' @param demo_data A data.table. demo should have a serious and death columns.
+#' @param adr_s A character string. The name of the adr column. Adr columns can be created with \code{\link{add_adr}} in a luda table.
+#' @param drug_s A character string. The name of the drug column. Drug columns can be created with \code{\link{add_drug}} in a luda table.
+#' @param tto_time_range Incertitude range of Time to onset, in days. Defaults to 1 as recommended by umc
 #'
-#' @return
+#' @return A one-row data.table with
+#' \itemize{
+#'   \item Counts of __overall__, __rch__, __inf__, and __rec__ cases (see details).
+#'   \item Number of cases with a time to onset `tto` available in all settings.
+#'   \item Median (interquartile range) time to onset in all settings.
+#' }
 #' @export
 #' @import dplyr data.table
 #'
 #' @examples
 #'
-#' rch_desc(luda_data = luda_test_all,
-#'          demo_data = demo_rch_test)
+#' rch_desc(luda_data = luda_,
+#'          demo_data = demo_rch_)
 rch_desc <- function(luda_data,
                      demo_data, # same here
                      adr_s = "a_colitis",
--- tests/testthat/test-desc_rch.R
+++ tests/testthat/test-desc_rch.R
@@ -1,39 +1,39 @@
 test_that("find proper counts on a known dataset", {
   rch_test <-
-    rch_desc(luda_data = luda_test_all,
-             demo_data = demo_rch_test,
+    rch_desc(luda_data = luda_,
+             demo_data = demo_rch_,
              adr_s = "a_colitis",
              drug_s = "pd1")
 
-  true_n_overall <- 10
+  true_n_overall <- 8
 
   expect_equal(
     rch_test$n_overall,
     true_n_overall
   )
 
-  true_n_rch <- 8
+  true_n_rch <- 6
 
   expect_equal(
     rch_test$n_rch,
     true_n_rch
   )
 
-  true_n_inf <- 7
+  true_n_inf <- 5
 
   expect_equal(
     rch_test$n_inf,
     true_n_inf
   )
 
-  true_n_rec <- 7
+  true_n_rec <- 5
 
   expect_equal(
     rch_test$n_rec,
     true_n_rec
   )
 
-  true_n_tto_avail_rch <- 4 # not 5, we're not collecting negative ttos
+  true_n_tto_avail_rch <- 3 # we're not collecting negative ttos
 
   expect_equal(
     rch_test$n_tto_avail_rch,
@@ -42,11 +42,11 @@ test_that("find proper counts on a known dataset", {
 
   true_tto_rch <-
     paste0(
-      (24 + 56) / 2, # median
+      (174), # median
       " (",
-      round(quantile(c(7, 24, 56, 66), .25)),
+      round(quantile(c(56, 174, 448), .25)),
       "-",
-      round(quantile(c(7, 24, 56, 66), .75)),
+      round(quantile(c(56, 174, 448), .75)),
       ")"
     )
 
