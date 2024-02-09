2110f1f32a610c690989e44cd5a95b1cdc71f751
cdolladille <cdolladille@hotmail.com>
2024-02-09 08:38:11 +0100

    trying to reduce data size and improve consistency

---
+++ data-raw/my_pkg_data.R
@@ -0,0 +1,5 @@
+## code to prepare `my_pkg_data` dataset goes here
+
+usethis::use_data(my_pkg_data, overwrite = TRUE)
+
+load("C:/Data/Unicloud/vigibase_ecl/rechallenge_ici/dm_data/demo_luda_ici.RData")
