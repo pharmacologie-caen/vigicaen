dba2ae2a2df2dd19a77eab4ca67d6fb290c03d4a
cdolladille <cdolladille@hotmail.com>
2024-02-22 23:06:48 +0100

    count cases rather than adr_ids (#50)

--- R/screen_adr.R
+++ R/screen_adr.R
@@ -4,7 +4,7 @@
 #'
 #' If `freq_threshold` is set to 0.05, all adverse drug reactions found at least in
 #'  5% of adrs in adr_data will be shown. That is not exactly the same as 5% of
-#'  cases (that would be found in demo).
+#'  cases. Counts are provided at *case* level (not adr level).
 #'
 #' @param adr_data A data.table, an adr data
 #' @param meddra A data.table, a meddra data
@@ -18,16 +18,29 @@
 #' @examples
 #' screen_adr(
 #'   adr_data = adr_,
-#'   meddra = meddra_
+#'   meddra = meddra_,
+#'   term_level = "pt"
+#' )
+#'
+#' screen_adr(
+#'   adr_data = adr_,
+#'   meddra = meddra_,
+#'   term_level = "hlt"
 #' )
 
 screen_adr <-
   function(
     adr_data,
     meddra,
-    term_level = "pt",
+    term_level =  c("soc", "hlgt", "hlt", "pt", "llt"),
     freq_threshold = 0.01
     ){
+
+    term_level <- match.arg(term_level)
+
+    term_level_name <- paste0(term_level, "_name")
+
+    term_sym <- rlang::sym(term_level_name)
 
     # check unique MedDRA_Ids from adr_data
     # and apply the frequency threshold.
@@ -37,17 +50,26 @@ screen_adr <-
         N > freq_threshold * nrow(adr_data)
       ]
 
+    umc_mid <-
+      adr_data[, ]
+
     # summarise, create a term to meddra_id table
 
     t_to_mid <-
-      meddra[llt_code %in% m_id_unique$MedDRA_Id,
-             .(pt_name, llt_code)]
+      rlang::eval_tidy(rlang::expr(
+        meddra[llt_code %in% m_id_unique$MedDRA_Id,
+             .(!!term_sym, llt_code)]
+      ))
 
-    m_id_counts <-
-      m_id_unique |>
+    # join that pt_names table to adr_data and count cases
+
+    n_case_counts <-
+      adr_data |>
       left_join(t_to_mid, by = c("MedDRA_Id" = "llt_code")) |>
-      summarise(n_cas = sum(N), .by = "pt_name") |>
+      select(UMCReportId, all_of(term_level_name)) |>
+      distinct() |>
+      summarise(n_cas = n(), .by = .env$term_level_name) |>
       arrange(desc(n_cas))
 
-    m_id_counts
+    n_case_counts
   }
