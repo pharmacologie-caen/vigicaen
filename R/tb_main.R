0d41927814ab5cb91348a91dcb12d04f00bb9257
cdolladille <cdolladille@hotmail.com>
2024-04-07 17:28:47 +0200

    tb_main and tb_who working to parquet, tb_sub isn't. Need to work al ot more

---
+++ R/tb_main.R
@@ -0,0 +1,435 @@
+#' Create main VigiBase ECL tables
+#'
+#' Transform .txt files to .parquet files
+#'
+#' Vigibase Extract Case Level is delivered as zipped text files, that you should
+#' transform to a more efficient format. Parquet format from arrow has many advantages:
+#' It works with out-of-memory data, which makes it possible to process Vigibase tables on
+#' a computer with not-so-much RAM. It is also lightweighted and standard across different
+#' langages.
+#' The function also creates variables in each table.
+#'
+#' @param path_base Character string, a directory containing vigibase txt tables. It is also the
+#' output directory.
+#' @param path_sub  Character string, a directory containing subsidiary tables.
+#'
+#' @importFrom stringr str_sub str_trim
+#'
+#' @export
+#'
+#' @seealso \code{\link{tb_who}} \code{\link{tb_sub}}
+#'
+#' @return .parquet files into the `path_base` directory (**including suspected duplicates tables**).
+#' Some columns are returned as `integer` (UMCReportId, Drug_Id, MedicinalProd_Id, Adr_Id, MedDRA_Id),
+#' and some columns as `numeric` (TimeToOnsetMin, TimeToOnsetMax)
+#' All other columns are `character`.
+#'
+#' @examples
+#'
+#' # --- Setting to try out ---- ####
+#'
+#' f_sets <-
+#'  list(
+#'    DEMO.txt = data.frame(f0= c("96548661   32194501051119460820")),
+#'    DRUG.txt = data.frame(f0 = c("70548965   8          4901354    064392080055011    31- 806")
+#'                          ),
+#'    LINK.txt = data.frame(f0 = c("2          654654     51---0.78991   0.98745    ",
+#'                                 "2          456456     51---6.98789   -          ")),
+#'    FOLLOWUP.txt = data.frame(f0 = c("0548978    0254687    ",
+#'                                     "7568798    4565321    ")),
+#'    ADR.txt = data.frame(f0 = c("96570161   14         100474561",
+#'                                "70578465   17         145078144")),
+#'    OUT.txt = data.frame(f0 = c("70547815   - N",
+#'                                "96575661   - Y")),
+#'    SRCE.txt = data.frame(f0 = c("4898765    1 ",
+#'                                 "9804562    1 ")),
+#'    IND.txt = data.frame(# 266 length
+#'      f0 = "780954     Cutaneous diseases due to other mycobacteria                                                                                                                                                                                                                   "
+#'      ),
+#'    SUSPECTEDDUPLICATES.txt = data.frame(f0 = c("789054     789542     ",
+#'                                                "780546     654352     "))
+#'  )
+#'
+#' tmp_folder <- tempdir()
+#'
+#' path_base <- paste0(tmp_folder, "/", "main", "/")
+#'
+#' dir.create(path_base)
+#'
+#' path_sub  <- paste0(tmp_folder, "/", "sub",  "/")
+#'
+#' dir.create(path_sub)
+#'
+#' purrr::iwalk(f_sets, function(d_, name_){
+#'   if(name_ == "SUSPECTEDDUPLICATES.txt"){
+#'     write.table(d_, file = paste0(path_sub, name_), row.names = FALSE, quote = FALSE, col.names = FALSE)
+#'   } else {
+#'     write.table(d_, file = paste0(path_base, name_), row.names = FALSE, quote = FALSE, col.names = FALSE)
+#'   }
+#' })
+#'
+#'  # ---- Running tb_main
+#'
+#'  tb_main(path_base = path_base,
+#'         path_sub  = path_sub)
+
+
+tb_main <-
+  function(path_base,
+           path_sub
+           ){
+
+    # helps working with the "here" package, or tempdir
+
+    if(!grepl("(/|\\\\)$", path_base, perl = TRUE)){
+      path_base <-
+        paste0(path_base, "/")
+
+      path_sub <-
+        paste0(path_sub, "/")
+    }
+
+    # same reading option for all but one (ind) file
+
+    reader <- function(file_name, folder = path_base){
+      arrow::read_delim_arrow(paste0(folder, file_name),
+                              col_names = FALSE,
+                              as_data_frame = FALSE,
+                              delim = "\t",
+                              schema = arrow::schema(f0 = arrow::utf8())
+      )
+    }
+
+    #  print progress text
+
+    texter <- function(msg, step){
+      cat(sprintf(
+        paste0("\r",
+               msg |>
+                 stringr::str_pad(
+                   side = "right",
+                   width = 28
+                 ),
+               step |>
+                 stringr::str_pad(
+                   side = "left",
+                   width = 5
+                 )
+          )
+      ))
+      }
+
+    # ---- demo ---- ####
+    texter("Read DEMO.txt", "3%%")
+
+    demo <- reader("DEMO.txt")
+
+    # ---- split
+    texter("Split demo", "6%%")
+
+    demo <-
+      demo |>
+      dplyr::transmute(
+        UMCReportId  = str_sub(f0, start = 1L,  end = 11L),
+        AgeGroup     = str_sub(f0, start = 12L, end = 12L),
+        Gender       = str_sub(f0, start = 13L, end = 13L),
+        DateDatabase = str_sub(f0, start = 14L, end = 21L),
+        Type         = str_sub(f0, start = 22L, end = 22L),
+        Region       = str_sub(f0, start = 23L, end = 23L),
+        FirstDateDatabase = str_sub(f0, start = 24L, end = 31L)
+      ) |>
+      dplyr::mutate(
+        UMCReportId = UMCReportId |>
+          str_trim() |>
+          as.integer()
+        ) |>
+      dplyr::compute()
+
+    # ---- write
+    texter("Write demo.parquet", "12%%")
+
+    arrow::write_parquet(demo,
+                  sink = paste0(path_base, "demo.parquet")
+    )
+
+    # ---- drug ---- ####
+    texter("Read DRUG.txt", "16%%")
+
+    drug <- reader("DRUG.txt")
+
+    # ---- split
+    texter("Split drug", "20%%")
+
+    drug <-
+      drug |>
+      dplyr::transmute(
+        UMCReportId = str_sub(f0, start = 1L, end = 11L),
+        Drug_Id     = str_sub(f0, start = 12L, end = 22L),
+        MedicinalProd_Id = str_sub(f0, start = 23L, end = 33L),
+        DrecNo      = str_sub(f0, start = 34L, end = 39L),
+        Seq1        = str_sub(f0, start = 40L, end = 41L),
+        Seq2        = str_sub(f0, start = 42L, end = 44L),
+        Route       = str_sub(f0, start = 45L, end = 46L),
+        Basis       = str_sub(f0, start = 47L, end = 47L),
+        Amount      = str_sub(f0, start = 48L, end = 52L),
+        AmountU     = str_sub(f0, start = 53L, end = 54L),
+        Frequency   = str_sub(f0, start = 55L, end = 56L),
+        FrequencyU  = str_sub(f0, start = 57L, end = 59L)
+        ) |>
+      dplyr::mutate(
+        dplyr::across(c(UMCReportId, Drug_Id, MedicinalProd_Id),
+               ~ .x |>
+          str_trim() |>
+          as.integer()
+        )
+      ) |>
+      dplyr::compute()
+
+    # ---- write
+    texter("Write drug.parquet", "27%%")
+
+    arrow::write_parquet(drug,
+                         sink = paste0(path_base, "drug.parquet")
+    )
+
+
+    # ---- followup ---- ####
+    texter("Read FOLLOWUP.txt", "30%%")
+
+    followup <- reader("FOLLOWUP.txt")
+
+    # ---- split
+    texter("Split followup", "32%%")
+
+    followup <-
+      followup |>
+      dplyr::transmute(
+        UMCReportId = str_sub(f0, start = 1L, end = 11L),
+        ReplacedUMCReportId = str_sub(f0, start = 12L, end = 22L)
+        ) |>
+      dplyr::mutate(
+        dplyr::across(c(UMCReportId, ReplacedUMCReportId),
+               ~ .x |>
+                 str_trim() |>
+                 as.integer()
+        )
+      ) |>
+      dplyr::compute()
+
+    # ---- write
+    texter("Write followup.parquet", "34%%")
+
+    arrow::write_parquet(followup,
+                         sink = paste0(path_base, "followup.parquet")
+    )
+
+    # ---- adr ---- ####
+    texter("Read ADR.txt", "36%%")
+
+    adr <- reader("ADR.txt")
+
+    # ---- split
+    texter("Split adr", "41%%")
+
+    adr <-
+      adr |>
+      dplyr::transmute(
+        UMCReportId = str_sub(f0, start = 1L, end = 11L),
+        Adr_Id      = str_sub(f0, start = 12L, end = 22L),
+        MedDRA_Id   = str_sub(f0, start = 23L, end = 30L),
+        Outcome     = str_sub(f0, start = 31L, end = 31L)
+      ) |>
+      dplyr::mutate(
+        dplyr::across(c(UMCReportId, Adr_Id, MedDRA_Id),
+                      ~ .x |>
+                        str_trim() |>
+                        as.integer()
+        )
+      ) |>
+      dplyr::compute()
+
+    # ---- write
+    texter("Write adr.parquet", "48%%")
+
+    arrow::write_parquet(adr,
+                         sink = paste0(path_base, "adr.parquet")
+    )
+
+    # ---- out ---- ####
+    texter("Read OUT.txt", "51%%")
+
+    out <- reader("OUT.txt")
+
+    # ---- split
+    texter("Split out", "53%%")
+
+    out <-
+      out |>
+      dplyr::transmute(
+        UMCReportId = str_sub(f0, start = 1L, end = 11L),
+        Seriousness = str_trim(str_sub(f0, start = 12L, end = 13L)),
+        Serious     = str_sub(f0, start = 14L, end = 14L)
+      ) |>
+      dplyr::mutate(
+        dplyr::across(c(UMCReportId),
+                      ~ .x |>
+                        str_trim() |>
+                        as.integer()
+        )
+      ) |>
+      dplyr::compute()
+
+    # ---- write
+    texter("Write out.parquet", "55%%")
+
+    arrow::write_parquet(out,
+                         sink = paste0(path_base, "out.parquet")
+    )
+
+    # ---- srce ---- ####
+    texter("Read SRCE.txt", "57%%")
+
+    srce <- reader("SRCE.txt")
+
+    # ---- split
+    texter("Split srce", "59%%")
+
+    srce <-
+      srce |>
+      dplyr::transmute(
+        UMCReportId = str_sub(f0, start = 1L, end = 11L),
+        Type        = str_trim(str_sub(f0, start = 12L, end = 13L))
+      ) |>
+      dplyr::mutate(
+        dplyr::across(c(UMCReportId),
+                      ~ .x |>
+                        str_trim() |>
+                        as.integer()
+        )
+      ) |>
+      dplyr::compute()
+
+    # ---- write
+    texter("Write srce.parquet", "61%%")
+
+    arrow::write_parquet(srce,
+                         sink = paste0(path_base, "srce.parquet")
+    )
+
+    # ---- link ---- ####
+    texter("Read LINK.txt", "63%%")
+
+    link <- reader("LINK.txt")
+
+    # ---- split
+    texter("Split link", "68%%")
+
+    link <-
+      link |>
+      dplyr::transmute(
+        Drug_Id        = str_sub(f0, start = 1L,  end = 11L),
+        Adr_Id         = str_sub(f0, start = 12L, end = 22L),
+        Dechallenge1   = str_sub(f0, start = 23L, end = 23L),
+        Dechallenge2   = str_sub(f0, start = 24L, end = 24L),
+        Rechallenge1   = str_sub(f0, start = 25L, end = 25L),
+        Rechallenge2   = str_sub(f0, start = 26L, end = 26L),
+        TimeToOnsetMin = str_sub(f0, start = 27L, end = 37L),
+        TimeToOnsetMax = str_sub(f0, start = 38L, end = 48L)
+      ) |>
+      dplyr::mutate(
+        dplyr::across(c(Drug_Id, Adr_Id),
+                      ~ .x |>
+                        str_trim() |>
+                        as.integer()
+                      ),
+        dplyr::across(c(TimeToOnsetMin, TimeToOnsetMax),
+                      ~
+                        .x |>
+                        str_trim() |>
+                        stringr::str_replace("^-$", "1568459784.65489") |>
+                        as.numeric()
+        ),
+        dplyr::across(c(TimeToOnsetMin, TimeToOnsetMax),
+                      ~ dplyr::if_else(.x == 1568459784.65489, NA_real_, .x))
+
+        )|>
+      dplyr::compute()
+
+    # ---- write
+    texter("Write link.parquet", "75%%")
+
+    arrow::write_parquet(link,
+                         sink = paste0(path_base, "link.parquet")
+    )
+
+    # ---- ind ---- ####
+    texter("Read IND.txt", "78%%")
+
+    ind <- arrow::read_delim_arrow(paste0(path_base, "IND.txt"),
+                                   col_names = FALSE,
+                                   as_data_frame = FALSE,
+                                   delim = "\t",
+                                   read_options =
+                                     arrow::csv_read_options(
+                                       column_names = "f0",
+                                       encoding = "ANSI_X3.4-1986")
+    )
+
+    # ---- split
+    texter("Split ind", "88%%")
+
+    ind <-
+      ind |>
+      dplyr::transmute(
+        Drug_Id    = str_sub(f0, start = 1L,  end = 11L),
+        Indication = str_trim(str_sub(f0, start = 12L, end = 266L))
+      ) |>
+      dplyr::mutate(
+        dplyr::across(c(Drug_Id),
+                      ~ .x |>
+                        str_trim() |>
+                        as.integer()
+        )
+      ) |>
+      dplyr::compute()
+
+    # ---- write
+    texter("Write ind.parquet", "92%%")
+
+    arrow::write_parquet(ind,
+                         sink = paste0(path_base, "ind.parquet")
+    )
+
+    # ---- suspectedduplicates ---- ####
+    texter("Read SUSPECTEDDUPLICATES.txt", "95%%")
+
+    suspdup <- reader("SUSPECTEDDUPLICATES.txt",
+                      folder = path_sub)
+
+    # ---- split
+    texter("Split suspdup", "97%%")
+
+    suspdup <-
+      suspdup |>
+      dplyr::transmute(
+        UMCReportId                = str_sub(f0, start = 1L,  end = 11L),
+        SuspectedduplicateReportId = str_sub(f0, start = 12L, end = 22L)
+      ) |>
+      dplyr::mutate(
+        dplyr::across(c(UMCReportId, SuspectedduplicateReportId),
+                      ~ .x |>
+                        str_trim() |>
+                        as.integer()
+        )
+      ) |>
+      dplyr::compute()
+
+    # ---- write
+    texter("Write suspdup.parquet", "99%%")
+
+    arrow::write_parquet(suspdup,
+                         sink = paste0(path_base, "suspdup.parquet")
+    )
+
+    texter("Done", "")
+  }
---
+++ R/tb_sub.R
@@ -0,0 +1,233 @@
+#' Create main VigiBase ECL tables
+#'
+#' Transform .txt files to .parquet files
+#'
+#' WHODrug is delivered as zipped text files, that you should
+#' transform to a more efficient format. Parquet format from arrow has many advantages:
+#' It works with out-of-memory data, which makes it possible to process tables on
+#' a computer with not-so-much RAM. It is also lightweighted and standard across different
+#' langages.
+#' The function also creates variables in each table. See \code{\link{tb_main}} for some running examples.
+#'
+#' @param path_who Character string, a directory containing whodrug txt tables. It is also the
+#' output directory.
+#'
+#' @importFrom stringr str_sub str_trim
+#'
+#' @seealso \code{\link{tb_main}} \code{\link{tb_sub}}
+#'
+#' @export
+#'
+#' @return .parquet files into the `path_who` directory.
+#' Some columns are returned as `integer` (all Id columns, including MedicinalProd_Id,
+#' with notable exception of DrecNo),
+#' and some columns as `numeric` (Quantity from ingredient table)
+#' All other columns are `character`.
+#'
+#' @examples
+#'
+#' # Use the examples from tb_main if you want to see these functions in action.
+#'
+#' path_who <- "/whodrug_directory/"
+#'
+#' ## NOT RUN ##
+#' # tb_who(path_who = path_who)
+#'
+
+tb_sub <- function(path_sub){
+# AgeGroup
+texter("Read AgeGroup_Lx.txt", "82%%")
+
+AgeGroup <- reader("AgeGroup_Lx.txt")
+AgeGroup <-
+  AgeGroup |>
+  dplyr::transmute(
+    AgeGroup = substr(f0, start=1, stop=1),
+    Code = substr(f0, start=2, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(AgeGroup, sink = paste0(path_sub, "AgeGroup.parquet"))
+
+# Dechallenge
+texter("Read Dechallenge_Lx.txt", "83%%")
+
+Dechallenge <- reader("Dechallenge_Lx.txt")
+Dechallenge <-
+  Dechallenge |>
+  dplyr::transmute(
+    Dechallenge1 = substr(f0, start=1, stop=1),
+    Code = substr(f0, start=2, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(Dechallenge, sink = paste0(path_sub, "Dechallenge.parquet"))
+
+# Dechallenge2
+texter("Read Dechallenge2_Lx.txt", "84%%")
+
+Dechallenge2 <- reader("Dechallenge2_Lx.txt")
+Dechallenge2 <-
+  Dechallenge2 |>
+  dplyr::transmute(
+    Dechallenge2 = substr(f0, start=1, stop=1),
+    Code = substr(f0, start=2, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(Dechallenge2, sink = paste0(path_sub, "Dechallenge2.parquet"))
+
+# FrequencyU
+texter("Read Frequency_Lx.txt", "85%%")
+
+Frequency <- reader("Frequency_Lx.txt")
+Frequency <-
+  Frequency |>
+  dplyr::transmute(
+    FrequencyU = substr(f0, start=1, stop=3),
+    Code = substr(f0, start=4, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(Frequency, sink = paste0(path_sub, "Frequency.parquet"))
+
+# Gender
+texter("Read Gender_Lx.txt", "86%%")
+
+Gender <- reader("Gender_Lx.txt")
+Gender <-
+  Gender |>
+  dplyr::transmute(
+    Gender = substr(f0, start=1, stop=1),
+    Code = substr(f0, start=2, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(Gender, sink = paste0(path_sub, "Gender.parquet"))
+
+# Notifier
+texter("Read Notifier_Lx.txt", "87%%")
+
+Notifier <- reader("Notifier_Lx.txt")
+Notifier <-
+  Notifier |>
+  dplyr::transmute(
+    Type = substr(f0, start=1, stop=2),
+    Code = substr(f0, start=3, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(Notifier, sink = paste0(path_sub, "Notifier.parquet"))
+
+# Outcome
+texter("Read Outcome_Lx.txt", "88%%")
+
+Outcome <- reader("Outcome_Lx.txt")
+Outcome <-
+  Outcome |>
+  dplyr::transmute(
+    Outcome = substr(f0, start=1, stop=1),
+    Code = substr(f0, start=2, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(Outcome, sink = paste0(path_sub, "Outcome.parquet"))
+
+# Rechallenge
+texter("Read Rechallenge_Lx.txt", "89%%")
+
+Rechallenge <- reader("Rechallenge_Lx.txt")
+Rechallenge <-
+  Rechallenge |>
+  dplyr::transmute(
+    Rechallenge1 = substr(f0, start=1, stop=1),
+    Code = substr(f0, start=2, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(Rechallenge, sink = paste0(path_sub, "Rechallenge.parquet"))
+
+# Rechallenge2
+texter("Read Rechallenge2_Lx.txt", "90%%")
+
+Rechallenge2 <- reader("Rechallenge2_Lx.txt")
+Rechallenge2 <-
+  Rechallenge2 |>
+  dplyr::transmute(
+    Rechallenge2 = substr(f0, start=1, stop=1),
+    Code = substr(f0, start=2, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(Rechallenge2, sink = paste0(path_sub, "Rechallenge2.parquet"))
+
+# Region
+texter("Read Region_Lx.txt", "91%%")
+
+Region <- reader("Region_Lx.txt")
+Region <-
+  Region |>
+  dplyr::transmute(
+    Region = substr(f0, start=1, stop=1),
+    Code = substr(f0, start=2, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(Region, sink = paste0(path_sub, "Region.parquet"))
+
+# RepBasis
+texter("Read RepBasis_Lx.txt", "92%%")
+
+RepBasis <- reader("RepBasis_Lx.txt")
+RepBasis <-
+  RepBasis |>
+  dplyr::transmute(
+    Basis = substr(f0, start=1, stop=1),
+    Code = substr(f0, start=2, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(RepBasis, sink = paste0(path_sub, "RepBasis.parquet"))
+
+# ReportType
+texter("Read ReportType_Lx.txt", "93%%")
+
+ReportType <- reader("ReportType_Lx.txt")
+ReportType <-
+  ReportType |>
+  dplyr::transmute(
+    ReportType = substr(f0, start=1, stop=1),
+    Code = substr(f0, start=2, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(ReportType, sink = paste0(path_sub, "ReportType.parquet"))
+
+# RouteOfAdm
+texter("Read RouteOfAdm_Lx.txt", "94%%")
+
+RouteOfAdm <- reader("RouteOfAdm_Lx.txt")
+RouteOfAdm <-
+  RouteOfAdm |>
+  dplyr::transmute(
+    Route = substr(f0, start=1, stop=2),
+    Code = substr(f0, start=3, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(RouteOfAdm, sink = paste0(path_sub, "RouteOfAdm.parquet"))
+
+# Seriousness
+texter("Read Seriousness_Lx.txt", "95%%")
+
+Seriousness <- reader("Seriousness_Lx.txt")
+Seriousness <-
+  Seriousness |>
+  dplyr::transmute(
+    Seriousness = substr(f0, start=1, stop=2),
+    Code = substr(f0, start=3, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(Seriousness, sink = paste0(path_sub, "Seriousness.parquet"))
+
+# SizeUnit
+texter("Read SizeUnit_Lx.txt", "96%%")
+
+SizeUnit <- reader("SizeUnit_Lx.txt")
+SizeUnit <-
+  SizeUnit |>
+  dplyr::transmute(
+    AmountU = substr(f0, start=1, stop=2),
+    Code = substr(f0, start=3, stop=nchar(f0))
+  ) |>
+  dplyr::compute()
+arrow::write_parquet(SizeUnit, sink = paste0(path_sub, "SizeUnit.parquet"))
+
+}
