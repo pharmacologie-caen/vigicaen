2307714c01dd1a004865c3f64d6ce955df99a4be
cdolladille <cdolladille@hotmail.com>
2024-07-16 11:44:07 +0200

    add tb_meddra and test

---
+++ R/tb_meddra.R
@@ -0,0 +1,159 @@
+#' Create MedDRA tables
+#'
+#' Transform .ascii files to .parquet files
+#'
+#' MedDRA is delivered as ascii files, that you should
+#' transform to a more efficient format. Parquet format from arrow has many advantages:
+#' It works with out-of-memory data, which makes it possible to process tables on
+#' a computer with not-so-much RAM. It is also lightweighted and standard across different
+#' langages.
+#' The function also creates variables in each table.
+#' You should note that NOT all MedDRA tables are processed with this function.
+#' Three tables are created: `meddra_hierarchy`, that respects the System Organ
+#' Class hierarchic classification. `smq_list` and `smq_content` for Standardized
+#' MedDRA Queries.
+#' **Caution** There tends to be small variations in the MedDRA ascii files structure.
+#' Last verified version on which this function is working is **26.1**.
+#'
+#' @param path_meddra Character string, a directory containing MedDRA ascii tables.
+#' It is also the output directory.
+#'
+#' @importFrom utils read.table
+#'
+#' @seealso \code{\link{tb_main}} \code{\link{tb_who}} \code{\link{tb_sub}}
+#'
+#' @export
+#'
+#' @return .parquet files into the `path_meddra` directory.
+#' Three tables: `meddra_hierarchy`, `smq_list`, and `smq_content.`
+#' Some columns are returned as `integer` (all `*_code` columns).
+#' All other columns are `character`.
+#'
+#' @examples
+#'
+#' # Use the examples from tb_main if you want to see these functions in action.
+#'
+#' path_meddra <- "/meddra_directory/"
+#'
+#' ## NOT RUN ##
+#' # tb_meddra(path_meddra = path_meddra)
+#'
+
+
+tb_meddra <-
+  function(path_meddra){
+
+    # ---- llt.asc ---- ####
+    texter("Read llt.asc", "15%%")
+
+    llt_table <- read.table(paste0(path_meddra, "llt.asc"),
+                            sep = "$",
+                            quote = "",
+                            fill = FALSE,
+                            col.names = c("llt_code",
+                                          "llt_name",
+                                          "pt_code",
+                                          "llt_whoart_code",
+                                          "llt_harts_code",
+                                          "llt_costart_sym",
+                                          "llt_icd9_code",
+                                          "llt_icd9cm_code",
+                                          "llt_icd10_code",
+                                          "llt_currency",
+                                          "llt_jart_code",
+                                          "empty_col"))
+
+    # ---- mdhier.asc ---- ####
+    texter("Read mdhier.asc", "30%%")
+
+    med_hierarchy <- read.table(paste0(path_meddra, "mdhier.asc"),
+                                sep = "$",
+                                quote = "",
+                                fill = FALSE,
+                                col.names = c("pt_code",
+                                              "hlt_code",
+                                              "hlgt_code",
+                                              "soc_code",
+                                              "pt_name",
+                                              "hlt_name",
+                                              "hlgt_name",
+                                              "soc_name",
+                                              "soc_abbrev",
+                                              "null_field",
+                                              "pt_soc_code",
+                                              "primary_soc_fg",
+                                              "empty_col"
+                                ))
+
+
+    med_hierarchy_llt <-
+      llt_table |>
+      dplyr::select(dplyr::all_of(c("llt_code", "llt_name", "pt_code"))) |>
+      dplyr::left_join(med_hierarchy, by = "pt_code")
+
+    # ---- write
+    texter("Write meddra_hierarchy.parquet", "45%%")
+
+    arrow::write_parquet(med_hierarchy_llt,
+                         sink = paste0(path_meddra, "meddra_hierarchy.parquet")
+    )
+
+    # SMQ #### ####
+
+    # ---- smq_list.asc ---- ####
+    texter("Read smq_list.asc", "60%%")
+
+    smq_list <- read.table(paste0(path_meddra, "smq_list.asc"),
+                           sep = "$",
+                           quote = "",
+                           comment.char = "",
+                           fill = FALSE,
+                           col.names = c("smq_code",
+                                         "smq_name",
+                                         "smq_level",
+                                         "smq_description",
+                                         "smq_source",
+                                         "smq_note",
+                                         "MedDRA_version",
+                                         "status",
+                                         "smq_algorithm",
+                                         "empty_col"))
+
+
+    # ---- write
+    texter("Write smq_list.parquet", "70%%")
+
+    arrow::write_parquet(smq_list,
+                         sink = paste0(path_meddra, "smq_list.parquet")
+    )
+
+    # ---- smq_content.asc ---- ####
+    texter("Read smq_content.asc", "80%%")
+
+    smq_content <- read.table(paste0(path_meddra, "smq_content.asc"),
+                              sep = "$",
+                              quote = "",
+                              comment.char = "",
+                              fill = FALSE,
+                              col.names = c("smq_code",
+                                            "term_code",
+                                            "term_level",
+                                            "term_scope",
+                                            "term_category",
+                                            "term_weight",
+                                            "term_status",
+                                            "term_addition_version",
+                                            "term_last_modified_version",
+                                            "empty_col"))
+
+    # ---- write
+    texter("Write smq_content.parquet", "90%%")
+
+    arrow::write_parquet(smq_content,
+                         sink = paste0(path_meddra, "smq_content.parquet")
+    )
+
+    texter("Done", "")
+
+  }
+
---
+++ tests/testthat/test-tb_meddra.R
@@ -0,0 +1,74 @@
+test_that("can process meddra tables", {
+  f_sets <-
+    list(
+      llt.asc = data.frame(f0 = c('486813518$A first LLT name$6548988$$$$$$$N$$',
+                                  '568798788$A second LLT name$47984764$$$$$$$Y$$')
+      ),
+      mdhier.asc = data.frame(f0 = c("6548988$84068465$98765468$8746878$A PT name$An HLT class$An HLGT class$A SOC class$A SOC class abrev$$12365489$Y$",
+"47984764$7852085$6852245$2358467$A 2nd PT name$HLT class$HLGT class$SOC class$SOC abrev$$10005329$Y$")),
+      smq_content.asc = data.frame(f0 = c("20486512$6548641$5$1$A$0$A$7.1$7.1$",
+                                          "20904441$98143546$5$1$A$0$A$7.1$7.1$")),
+      smq_list.asc = data.frame(f0 = c("20486512$The SMQ name (SMQ)$1$Long text describing this SMQ to provide context to the user$References$$26.1$A$N$",
+"20904441$Another name (SMQ)$1$Another long text$References$$26.1$A$N$")
+)
+    )
+
+  tmp_folder <- tempdir()
+  path_meddra <- paste0(tmp_folder, "/", "meddra", "/")
+  dir.create(path_meddra)
+
+  purrr::iwalk(f_sets, function(d_, name_){
+      write.table(d_, file = paste0(path_meddra, name_), row.names = FALSE,
+                  quote = FALSE, col.names = FALSE)
+    })
+
+  tb_meddra(path_meddra)
+
+  meddra_res <-
+    arrow::read_parquet(paste0(path_meddra, "meddra_hierarchy.parquet"))
+
+  smq_list_res <-
+    arrow::read_parquet(paste0(path_meddra, "smq_list.parquet"))
+
+
+  smq_content_res <-
+    arrow::read_parquet(paste0(path_meddra, "smq_content.parquet"))
+
+  expect_equal(
+    meddra_res$llt_code,
+    c(486813518L, 568798788L)
+  )
+
+  expect_equal(
+    meddra_res$llt_name,
+    c("A first LLT name",
+      "A second LLT name")
+  )
+
+  expect_equal(
+    meddra_res$pt_name,
+    c("A PT name",
+      "A 2nd PT name")
+  )
+
+  expect_equal(
+    meddra_res$pt_code,
+    c(6548988L, 47984764L)
+  )
+
+  expect_equal(
+    smq_list_res$smq_code,
+    c(20486512L, 20904441L)
+  )
+
+  expect_equal(
+    smq_content_res$smq_code,
+    c(20486512L, 20904441L)
+  )
+
+  expect_equal(
+    smq_content_res$term_code,
+    c(6548641L, 98143546L)
+  )
+})
+
