1b90cd7e88c7e45372a790c5127b92e788568394
cdolladille <cdolladille@hotmail.com>
2024-07-17 00:40:44 +0200

    fix tb_sub description

--- R/tb_sub.R
+++ R/tb_sub.R
@@ -1,13 +1,10 @@
-#' Create main VigiBase ECL tables
+#' Create subsidiary VigiBase ECL tables
 #'
 #' Transform .txt files to .parquet files
 #'
-#' WHODrug is delivered as zipped text files, that you should
-#' transform to a more efficient format. Parquet format from arrow has many advantages:
-#' It works with out-of-memory data, which makes it possible to process tables on
-#' a computer with not-so-much RAM. It is also lightweighted and standard across different
-#' langages.
-#' The function also creates variables in each table. See \code{\link{tb_main}} for some running examples.
+#' Subsidiary files in VigiBase provide context to many numerically stored variables.
+#' Most important file here is the suspected duplicates table, but it is processed
+#' in tb_main. More documentation can be found in \code{\link{tb_main}}.
 #'
 #' @param path_sub Character string, a directory containing whodrug txt tables. It is also the
 #' output directory.
@@ -18,25 +15,21 @@
 #'
 #' @export
 #'
-#' @return .parquet files into the `path_who` directory.
-#' Some columns are returned as `integer` (all Id columns, including MedicinalProd_Id,
-#' with notable exception of DrecNo),
-#' and some columns as `numeric` (Quantity from ingredient table)
-#' All other columns are `character`.
+#' @return .parquet files into the `path_sub` directory.
 #'
 #' @examples
 #'
 #' # Use the examples from tb_main if you want to see these functions in action.
 #'
-#' path_who <- "/whodrug_directory/"
+#' path_who <- "/vigibase_subsidiary_files/"
 #'
 #' ## NOT RUN ##
-#' # tb_who(path_who = path_who)
+#' # tb_sub(path_sub = path_sub)
 #'
 
 tb_sub <- function(path_sub){
 # AgeGroup
-texter("Read AgeGroup_Lx.txt", "82%%")
+texter("Read AgeGroup_Lx.txt", "1%%")
 
 AgeGroup <- reader("AgeGroup_Lx.txt")
 AgeGroup <-
@@ -49,7 +42,7 @@ AgeGroup <-
 arrow::write_parquet(AgeGroup, sink = paste0(path_sub, "AgeGroup.parquet"))
 
 # Dechallenge
-texter("Read Dechallenge_Lx.txt", "83%%")
+texter("Read Dechallenge_Lx.txt", "7%%")
 
 Dechallenge <- reader("Dechallenge_Lx.txt")
 Dechallenge <-
@@ -62,7 +55,7 @@ Dechallenge <-
 arrow::write_parquet(Dechallenge, sink = paste0(path_sub, "Dechallenge.parquet"))
 
 # Dechallenge2
-texter("Read Dechallenge2_Lx.txt", "84%%")
+texter("Read Dechallenge2_Lx.txt", "13%%")
 
 Dechallenge2 <- reader("Dechallenge2_Lx.txt")
 Dechallenge2 <-
@@ -75,7 +68,7 @@ Dechallenge2 <-
 arrow::write_parquet(Dechallenge2, sink = paste0(path_sub, "Dechallenge2.parquet"))
 
 # FrequencyU
-texter("Read Frequency_Lx.txt", "85%%")
+texter("Read Frequency_Lx.txt", "19%%")
 
 Frequency <- reader("Frequency_Lx.txt")
 Frequency <-
@@ -88,7 +81,7 @@ Frequency <-
 arrow::write_parquet(Frequency, sink = paste0(path_sub, "Frequency.parquet"))
 
 # Gender
-texter("Read Gender_Lx.txt", "86%%")
+texter("Read Gender_Lx.txt", "25%%")
 
 Gender <- reader("Gender_Lx.txt")
 Gender <-
@@ -101,7 +94,7 @@ Gender <-
 arrow::write_parquet(Gender, sink = paste0(path_sub, "Gender.parquet"))
 
 # Notifier
-texter("Read Notifier_Lx.txt", "87%%")
+texter("Read Notifier_Lx.txt", "31%%")
 
 Notifier <- reader("Notifier_Lx.txt")
 Notifier <-
@@ -114,7 +107,7 @@ Notifier <-
 arrow::write_parquet(Notifier, sink = paste0(path_sub, "Notifier.parquet"))
 
 # Outcome
-texter("Read Outcome_Lx.txt", "88%%")
+texter("Read Outcome_Lx.txt", "37%%")
 
 Outcome <- reader("Outcome_Lx.txt")
 Outcome <-
@@ -127,7 +120,7 @@ Outcome <-
 arrow::write_parquet(Outcome, sink = paste0(path_sub, "Outcome.parquet"))
 
 # Rechallenge
-texter("Read Rechallenge_Lx.txt", "89%%")
+texter("Read Rechallenge_Lx.txt", "43%%")
 
 Rechallenge <- reader("Rechallenge_Lx.txt")
 Rechallenge <-
@@ -140,7 +133,7 @@ Rechallenge <-
 arrow::write_parquet(Rechallenge, sink = paste0(path_sub, "Rechallenge.parquet"))
 
 # Rechallenge2
-texter("Read Rechallenge2_Lx.txt", "90%%")
+texter("Read Rechallenge2_Lx.txt", "49%%")
 
 Rechallenge2 <- reader("Rechallenge2_Lx.txt")
 Rechallenge2 <-
@@ -153,7 +146,7 @@ Rechallenge2 <-
 arrow::write_parquet(Rechallenge2, sink = paste0(path_sub, "Rechallenge2.parquet"))
 
 # Region
-texter("Read Region_Lx.txt", "91%%")
+texter("Read Region_Lx.txt", "55%%")
 
 Region <- reader("Region_Lx.txt")
 Region <-
@@ -166,7 +159,7 @@ Region <-
 arrow::write_parquet(Region, sink = paste0(path_sub, "Region.parquet"))
 
 # RepBasis
-texter("Read RepBasis_Lx.txt", "92%%")
+texter("Read RepBasis_Lx.txt", "61%%")
 
 RepBasis <- reader("RepBasis_Lx.txt")
 RepBasis <-
@@ -179,7 +172,7 @@ RepBasis <-
 arrow::write_parquet(RepBasis, sink = paste0(path_sub, "RepBasis.parquet"))
 
 # ReportType
-texter("Read ReportType_Lx.txt", "93%%")
+texter("Read ReportType_Lx.txt", "67%%")
 
 ReportType <- reader("ReportType_Lx.txt")
 ReportType <-
@@ -192,7 +185,7 @@ ReportType <-
 arrow::write_parquet(ReportType, sink = paste0(path_sub, "ReportType.parquet"))
 
 # RouteOfAdm
-texter("Read RouteOfAdm_Lx.txt", "94%%")
+texter("Read RouteOfAdm_Lx.txt", "75%%")
 
 RouteOfAdm <- reader("RouteOfAdm_Lx.txt")
 RouteOfAdm <-
@@ -205,7 +198,7 @@ RouteOfAdm <-
 arrow::write_parquet(RouteOfAdm, sink = paste0(path_sub, "RouteOfAdm.parquet"))
 
 # Seriousness
-texter("Read Seriousness_Lx.txt", "95%%")
+texter("Read Seriousness_Lx.txt", "88%%")
 
 Seriousness <- reader("Seriousness_Lx.txt")
 Seriousness <-
