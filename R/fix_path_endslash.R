25dd0a4554702c62d5a26f63fac8e63e055a63df
Charles Dolladille <cdolladille@hotmail.com>
2024-11-15 16:38:26 +0100

    fix #91 end slash and existence checking of args path_*

---
+++ R/fix_path_endslash.R
@@ -0,0 +1,25 @@
+#' Internal: Fix path end slash
+#'
+#' This function adds a slash at the end of a path if it is missing.
+#'
+#' Note, the function does NOT check that the path exists.
+#' This internal fixer is notably useful to make `here::here()` use more fluent.
+#' And also with `tempdir()`.
+#'
+#' @param .path A character string with the path to be fixed.
+#'
+#' @return A character string with the path, fixed with its end slash.
+#'
+#' @seealso [tb_main()], [tb_who()], [tb_meddra()], [tb_custom()], [dt_parquet()]
+#'
+#' @examples
+#' vigicaen:::fix_path_endslash("C:/Users/username/Documents")
+
+fix_path_endslash <-
+  function(.path){
+    if(!grepl("(/|\\\\)$", .path, perl = TRUE)){
+      paste0(.path, "/")
+    } else {
+      .path
+    }
+  }
