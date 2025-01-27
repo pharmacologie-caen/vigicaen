c89432dd3bc5fdedca7adc8492d281d7c0918434
Charles Dolladille <cdolladille@hotmail.com>
2025-01-27 09:13:39 +0100

    renaming compute_or_abcd to compute_dispro

--- tests/testthat/test-compute_dispro.R
+++ tests/testthat/test-compute_dispro.R
@@ -1,24 +1,26 @@
 test_that("computation is accurate", {
-  expect_snapshot({
+  expect_message(expect_message({
     demo <-
-      demo_ %>%
-      add_drug(d_code = ex_$d_drecno, drug_data = drug_) %>%
+      demo_  |>
+      add_drug(d_code = ex_$d_drecno, drug_data = drug_) |>
       add_adr(a_code = ex_$a_llt, adr_data = adr_)
-  })
+  }))
 
   res <-
     demo %>%
-    compute_or_abcd(
+    compute_dispro(
       y = "a_colitis",
-      x = "nivolumab"
+      x = "nivolumab",
+      export_raw_values = TRUE
     )
 
   res_a <-
     demo |>
     arrow::as_arrow_table() |>
-    compute_or_abcd(
+    compute_dispro(
       y = "a_colitis",
-      x = "nivolumab"
+      x = "nivolumab",
+      export_raw_values = TRUE
     )
 
   exp_res <- rlang::list2(
@@ -27,7 +29,7 @@ test_that("computation is accurate", {
   )
 
   expect_equal(
-    res[["orl"]],
+    res[["or"]],
     exp_res[["or"]]
   )
 
@@ -48,17 +50,19 @@ test_that("handles 0 cases in y/x combination", {
 
   res_misb <-
     demo %>%
-    compute_or_abcd(
+    compute_dispro(
       y = "a_colitis",
-      x = "nivolumab"
+      x = "nivolumab",
+      export_raw_values = TRUE
     )
 
   res_misb_a <-
     demo |>
     arrow::as_arrow_table() |>
-    compute_or_abcd(
+    compute_dispro(
       y = "a_colitis",
-      x = "nivolumab"
+      x = "nivolumab",
+      export_raw_values = TRUE
     )
 
   expect_equal(
@@ -76,17 +80,19 @@ test_that("handles 0 cases in y/x combination", {
 
   res_misc <-
     demo_misc %>%
-    compute_or_abcd(
+    compute_dispro(
       y = "a_colitis",
-      x = "nivolumab"
+      x = "nivolumab",
+      export_raw_values = TRUE
     )
 
   res_misc_a <-
     demo_misc |>
     arrow::as_arrow_table() |>
-    compute_or_abcd(
+    compute_dispro(
       y = "a_colitis",
-      x = "nivolumab"
+      x = "nivolumab",
+      export_raw_values = TRUE
     )
 
 
@@ -105,17 +111,19 @@ test_that("handles 0 cases in y/x combination", {
 
   res_misa <-
     demo_misa %>%
-    compute_or_abcd(
+    compute_dispro(
       y = "a_colitis",
-      x = "nivolumab"
+      x = "nivolumab",
+      export_raw_values = TRUE
     )
 
   res_misa_a <-
     demo_misa |>
     arrow::as_arrow_table() |>
-    compute_or_abcd(
+    compute_dispro(
       y = "a_colitis",
-      x = "nivolumab"
+      x = "nivolumab",
+      export_raw_values = TRUE
     )
 
 
@@ -135,17 +143,19 @@ test_that("handles 0 cases in y/x combination", {
 
   res_misd <-
     demo_misd %>%
-    compute_or_abcd(
+    compute_dispro(
       y = "a_colitis",
-      x = "nivolumab"
+      x = "nivolumab",
+      export_raw_values = TRUE
     )
 
   res_misd_a <-
     demo_misd |>
     arrow::as_arrow_table() |>
-    compute_or_abcd(
+    compute_dispro(
       y = "a_colitis",
-      x = "nivolumab"
+      x = "nivolumab",
+      export_raw_values = TRUE
     )
 
 
@@ -176,7 +186,7 @@ test_that("vectorization works inside and outside the function", {
       "a_pneumonitis"
       )
 
-  true_orl <-
+  true_or <-
     c("1.88",
       "1.75",
       "0.94",
@@ -190,9 +200,10 @@ test_that("vectorization works inside and outside the function", {
          purrr::map(
            function(an_adr)
              demo |>
-             compute_or_abcd(
+             compute_dispro(
                y = an_adr,
-               x = a_drug
+               x = a_drug,
+               export_raw_values = TRUE
                )
            ) |>
          purrr::list_rbind()
@@ -209,9 +220,10 @@ test_that("vectorization works inside and outside the function", {
             function(an_adr)
               demo  |>
               arrow::as_arrow_table() |>
-              compute_or_abcd(
+              compute_dispro(
                 y = an_adr,
-                x = a_drug
+                x = a_drug,
+                export_raw_values = TRUE
               )
           ) |>
           purrr::list_rbind()
@@ -221,17 +233,19 @@ test_that("vectorization works inside and outside the function", {
 
   vect_inside <-
     demo |>
-    compute_or_abcd(
+    compute_dispro(
       y = many_adrs,
-      x = many_drugs
+      x = many_drugs,
+      export_raw_values = TRUE
       )
 
   vect_inside_a <-
     demo |>
     arrow::as_arrow_table() |>
-    compute_or_abcd(
+    compute_dispro(
       y = many_adrs,
-      x = many_drugs
+      x = many_drugs,
+      export_raw_values = TRUE
     )
 
   expect_equal(vect_outside, vect_inside)
@@ -240,8 +254,8 @@ test_that("vectorization works inside and outside the function", {
 
   expect_equal(vect_inside,  vect_inside_a)
 
-  expect_equal(vect_inside$orl,
-               true_orl)
+  expect_equal(vect_inside$or,
+               true_or)
 
 
 })
@@ -265,9 +279,10 @@ test_that("works with large numbers", {
 
   r1 <-
     demo |>
-    compute_or_abcd(
+    compute_dispro(
       y = "event1",
-      x = "drug1"
+      x = "drug1",
+      export_raw_values = TRUE
     )
 
   z_val <- qnorm(1 - 0.05 / 2)
@@ -276,31 +291,37 @@ test_that("works with large numbers", {
     dplyr::tibble(
       y = "event1",
       x = "drug1",
+      n_obs = as.numeric(2000000),
+      n_exp = 6000000,
+      or    = "0.06",
+      or_ci  = "(0.06-0.06)",
+      ci_level = "95%",
+
       a = as.numeric(2000000),
       b = as.numeric(8000000),
       c = as.numeric(16000000),
       d = as.numeric(4000000),
-      n_exp = 6000000,
       std_er = sqrt((1 / .data$a) +
                       (1 / .data$b) +
                       (1 / .data$c) +
                       (1 / .data$d)
       ),
-      or     =  .data$a * .data$d / (.data$b * .data$c),
-      low_ci  = .data$or * exp(- .env$z_val * .data$std_er),
-      up_ci  = .data$or * exp(+ .env$z_val * .data$std_er),
-      orl    = "0.06",
-      or_ci  = "(0.06-0.06)",
       ic = log((.data$a + .5) / (.data$n_exp + .5), base = 2),
       ic_tail = ic_tail(
         n_obs = .data$a,
         n_exp = .data$n_exp,
         p = 0.05 / 2
       ),
-      ci_level = "95%",
+      or_raw     =  .data$a * .data$d / (.data$b * .data$c),
+      low_ci  = .data$or_raw * exp(- .env$z_val * .data$std_er),
+      up_ci  = .data$or_raw * exp(+ .env$z_val * .data$std_er),
       signif_or = 0,
       signif_ic = 0
-    )
+    ) |>
+    dplyr::select(dplyr::all_of(
+      c("y", "x", "n_obs", "n_exp", "or", "or_ci", "ic", "ic_tail", "ci_level",
+        "a", "b", "c", "d", "std_er", "or_raw", "low_ci", "up_ci",
+        "signif_or", "signif_ic")))
 
 
   expect_equal(
@@ -313,4 +334,56 @@ test_that("works with large numbers", {
     -1.58
   )
 
+})
+
+test_that("short nice format and min_n_obs format works", {
+  expect_message(expect_message({
+    demo <-
+      demo_ %>%
+      add_drug(d_code = ex_$d_drecno, drug_data = drug_) %>%
+      add_adr(a_code = ex_$a_llt, adr_data = adr_)
+  }))
+
+  res <-
+    demo %>%
+    compute_dispro(
+      y = "a_colitis",
+      x = "nivolumab",
+      export_raw_values = FALSE
+    ) |>
+    dplyr::mutate(
+      ic = cff(ic, dig = 3),
+      ic_tail = cff(ic_tail, dig = 4)
+    )
+
+  res_true <-
+    dplyr::tibble(
+      y = "a_colitis",
+      x = "nivolumab",
+      n_obs = 44,
+      n_exp = 31.2,
+      or = "1.88",
+      or_ci = "(1.23-2.88)",
+      ic = "0.489",
+      ic_tail = "0.0314",
+      ci_level = "95%"
+    )
+
+  expect_equal(res, res_true)
+
+  res <-
+    demo %>%
+    compute_dispro(
+      y = c("a_colitis", "a_pneumonitis"),
+      x = "nivolumab",
+      na_format = "wow",
+      min_n_obs = 43,
+      export_raw_values = TRUE
+    )
+
+  expect_equal(
+    res |> dplyr::filter(y == "a_pneumonitis") |>
+      dplyr::pull(or),
+    NA_character_
+  )
 })
